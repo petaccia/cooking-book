d898cdcdaf11b265da6892a9610274396f0732d1
PETACCIA <petaccia.seb@gmail.com>
2024-06-17 14:23:55 +0200

    Suppression du login facebook en back

--- backend/src/passport/passportLogin.js
+++ backend/src/passport/passportLogin.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const facebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models/userModel'); 
 
 passport.use(new GoogleStrategy({
@@ -26,28 +25,6 @@ async (accessToken, refreshToken, profile, done) => {
   }
 }));
 
-passport.use(new facebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: 'http://localhost:3000/auth/facebook/callback'
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ facebookId: profile.id });
-
-    if (!user) {
-      user = new User({
-        facebookId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value,
-      });
-      await user.save();
-    }
-    return done(null, user);
-  } catch (error) {
-    return done(error, false);
-  }
-}));
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -56,9 +33,9 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((id, done) => {
   User.findById(id)
     .then(user => {
-      done(null, user); // Passes the found user to serializeUser
+      done(null, user); 
     })
     .catch(err => {
-      done(err, null); // Passes the error to serializeUser
+      done(err, null); 
     });
 });
