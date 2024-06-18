a9a07a67c440b4589632854cb97a6b0a705c6e9d
PETACCIA <petaccia.seb@gmail.com>
2024-06-18 09:36:38 +0200

    Création de la listes des ingredients et envoie dans la base de données

---
+++ backend/seed/seedIngredients.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Ingredient = require('../src/models/ingredientModel');
+const connectDB = require('../src/database/config');
+const ingredientsData = require('../data/ingredientsData');
+
+
+const createIngredients = async () => {
+  try {
+    await connectDB();
+    for (const ingredient of ingredientsData) {
+      const newIngredient = new Ingredient(ingredient);
+      await newIngredient.save();
+      console.log(`Enregistrement de l'ingrédient : ${newIngredient.name}`);
+    }
+    console.log('Ingrédients enregistrées avec succès !');
+  } catch (error) {
+    console.error('Erreur lors de l\'enregistrement des ingrédients :', error);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+createIngredients();
