cd29fc9675e346e31f4b47e9f6f03fd82c573afb
PETACCIA <petaccia.seb@gmail.com>
2024-06-18 09:37:47 +0200

    Création de la listes des ingredients et envoie dans la base de données

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     console.log(`Connecting to MongoDB...`);
+    
     await mongoose.connect(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eo76lnd.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`);
     console.log(`Connected to MongoDB: cooking-book`);
