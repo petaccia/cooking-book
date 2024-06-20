c94bc30aaaf0a4b7092cfa8ed41af377f4b4aa7a
PETACCIA <petaccia.seb@gmail.com>
2024-06-20 14:05:32 +0200

    Création du composant input

---
+++ frontend/src/components/form/input.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Input = ({ id, label, type = 'text', register, error }) => {
+  return (
+    <div className="w-full px-2 mb-4">
+      <label htmlFor={id} className="block text-gray-700 font-bold mb-2">
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        {...register}
+        className={`w-full p-2 border border-gray-300 rounded ${error ? 'border-red-500' : ''}`}
+      />
+      {error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
+    </div>
+  );
+};
+
+export default Input;
