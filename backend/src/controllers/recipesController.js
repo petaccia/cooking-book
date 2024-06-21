4ccbc6b77a29095a579a011cb203e17d8d354896
PETACCIA <petaccia.seb@gmail.com>
2024-06-21 16:06:06 +0200

    Création de la fonction pour lire une recette dans le controller et aussi de la routes pour le backend

--- backend/src/controllers/recipesController.js
+++ backend/src/controllers/recipesController.js
@@ -9,6 +9,15 @@ const getRecipes = async (req, res) => {
   }
 };
 
+const getRecipe = async (req, res) => {
+  try {
+    const recipe = await recipes.findById(req.params.id);
+    res.status(200).json(recipe);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 
-module.exports = { getRecipes };
+
+module.exports = { getRecipes , getRecipe };
