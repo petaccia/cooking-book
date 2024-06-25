3c68d68fa6e18fe420c0c7803c2e5413dc06bce7
PETACCIA <petaccia.seb@gmail.com>
2024-06-25 15:09:11 +0200

    modification pour récupération des données du back des recettes

--- backend/src/controllers/recipesController.js
+++ backend/src/controllers/recipesController.js
@@ -1,23 +1,31 @@
-const recipes = require("../models/recipeModel");
+const Recipes = require("../models/recipeModel");
+const Ingredients = require("../models/ingredientModel");
 
-const getRecipes = async (req, res) => {
+
+exports.getRecipes = async (req, res) => {
   try {
-    const recipesAll = await recipes.find();
+    const recipesAll = await Recipes.find();
     res.status(200).json(recipesAll);
   } catch (err) {
     res.status(500).json(err);
   }
 };
 
-const getRecipe = async (req, res) => {
+
+exports.getRecipeById = async (req, res) => {
   try {
-    const recipe = await recipes.findById(req.params.id);
+    const recipe = await Recipes.findById(req.params.id)
+      .populate('ingredients')
+      .populate('author', 'username'); // Nous gardons la population de l'auteur
+
+    if (!recipe) {
+      return res.status(404).json({ message: "Recette non trouvée" });
+    }
+
     res.status(200).json(recipe);
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de la récupération de la recette", error: error.message });
   }
 };
 
 
-
-module.exports = { getRecipes , getRecipe };
