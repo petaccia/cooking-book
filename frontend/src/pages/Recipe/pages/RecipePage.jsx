70304e3a0f665433794a23176032a145a2be9746
PETACCIA <petaccia.seb@gmail.com>
2024-06-26 11:16:05 +0200

    Modification de la page de la recette pour rajouter l'icon utilisateur et icône du temps pour la recette

--- frontend/src/pages/Recipe/pages/RecipePage.jsx
+++ frontend/src/pages/Recipe/pages/RecipePage.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getRecipeById } from '../../../api';
 import Loader from '../../../components/loader/Loader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus, faClock} from '@fortawesome/free-solid-svg-icons';
+
 
 const RecipePage = () => {
-  const { id } = useParams(); // Récupération de l'identifiant de la recette depuis l'URL
+  const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -16,10 +19,10 @@ const RecipePage = () => {
           const data = await getRecipeById(id);
           setRecipe(data);
         } else {
-          throw new Error('Invalid recipe ID');
+          throw new Error("L'identifiant de la recette est manquant");
         }
       } catch (error) {
-        setError('Erreur lors du chargement de la recette. Veuillez réessayer plus tard.');
+        setError("Erreur lors du chargement de la recette. Veuillez réessayer plus tard.");
       } finally {
         setLoading(false);
       }
@@ -32,24 +35,38 @@ const RecipePage = () => {
   if (error) return <p className="text-red-500">{error}</p>;
 
   return (
-    <div className="recipe-container max-w-screen-lg mx-auto p-4 bg-slate-100 rounded-lg shadow-lg shadow-gray-900 overflow-hidden  ">
+    <div className="recipe-container max-w-screen-lg mx-auto p-4 bg-slate-100 rounded-lg shadow-lg overflow-hidden">
       {recipe && (
-        <div className="recipe-content bg-white rounded-lg shadow-lg shadow-gray-900 p-6">
+        <div className="recipe-content bg-white rounded-lg shadow-lg p-6">
           <h1 className="recipe-title text-4xl font-bold mb-4 text-stone-800">{recipe.title}</h1>
+          
+          {/* Nouvelle section pour les informations générales */}
+          <div className="recipe-info flex justify-between items-center mb-4 text-stone-600">
+            <div className="author flex items-center">
+              <FontAwesomeIcon icon={faUserPlus} className="mr-2" />
+              <span>Par {recipe.author[0].pseudo||recipe.author[0].name}</span>
+            </div>
+            <div className="cooktime flex items-center">
+              <FontAwesomeIcon icon={faClock} className="mr-2" />
+              <span>Temps de cuisson : {recipe.tcooktime} minutes</span>
+            </div>
+          </div>
+
           <img src={recipe.image} alt={recipe.title} className="recipe-image w-full h-auto mb-4 object-cover rounded-lg" />
           <p className="recipe-description text-lg mb-4 text-stone-500 italic font-medium">{recipe.description}</p>
-          <div className="recipe-ingredients mb-6 border-b-2 pb-2 border-stone-800">
-            <h2 className="ingredients-title text-2xl font-semibold mb-6 border-b-2 pb-2 border-stone-800 ">Ingrédients</h2>
-            <ul className="ingredients-list list-disc list-inside mx-2 h-40 overflow-y-auto w-full flex flex-col ">
+          <div className="recipe-ingredients mb-4 border-b-2 pb-2 border-stone-800">
+            <h2 className="ingredients-title text-2xl font-semibold mb-6 border-b-2 pb-2 border-stone-800">Ingrédients</h2>
+            <div className="ingredients-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
               {recipe.ingredients.map((ingredient, index) => (
-                <li key={index} className="ingredient-item  mb-2 text-stone-500 font-medium text-lg leading-normal h-10  flex  justify-between items-center px-4">
-                  <span>{ingredient.name}</span>
-                  {ingredient.image && (
-                    <img src={ingredient.image} alt={ingredient.name} className="ingredient-image w-10 h-10 object-cover ml-4 rounded-full  border-2 border-stone-300" />
+                <div key={index} className="ingredient-card bg-white rounded-lg shadow-md p-4 flex flex-col items-center">
+                  {ingredient.ingredientId.image && (
+                    <img src={ingredient.ingredientId.image} alt={ingredient.ingredientId.name} className="ingredient-image w-20 h-20 object-cover mb-2 rounded-full border-2 border-stone-300" />
                   )}
-                </li>
+                  <span className="ingredient-name text-stone-800 font-semibold text-lg">{ingredient.ingredientId.name}</span>
+                  <span className="ingredient-quantity text-stone-500 font-medium text-sm">{ingredient.quantity} {ingredient.ingredientId.unit}</span>
+                </div>
               ))}
-            </ul>
+            </div>
           </div>
           <div className="recipe-instructions">
             <h2 className="instructions-title text-2xl font-semibold mb-2 border-b-2 pb-2 border-stone-800">Instructions</h2>
