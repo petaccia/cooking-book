75b9e391f46e72da314475041124572aa0393c4d
PETACCIA <petaccia.seb@gmail.com>
2024-06-27 11:33:38 +0200

    Création du controller pour que l'utilisateur puisse ajouter une recette en favorie et aussi les voir

---
+++ backend/src/controllers/favoriteRecipeController.js
@@ -0,0 +1,29 @@
+const FavoriteRecipe = require("../models/favoriteRecipeModel");
+
+
+
+exports.getFavoriteRecipe = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const favoriteRecipes = await FavoriteRecipe.find({ user: userId });
+    res.status(200).json(favoriteRecipes);
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de la recherche des recettes favorites", error: error.message });
+  }
+}
+
+
+
+exports.addFavoriteRecipe = async (req, res) => {
+  try {
+    const { userId, recipeId } = req.body;
+    const favoriteRecipe = new FavoriteRecipe({
+      user: userId,
+      recipe: recipeId,
+    });
+    await favoriteRecipe.save();
+    res.status(201).json({ message: "Recette enregistrée" });
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de l'enregistrement de la recette", error: error.message });
+  }
+}
