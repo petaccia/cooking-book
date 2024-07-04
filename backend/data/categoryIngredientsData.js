0b50f9c4cdeea467812409f77c9b04e93c6fc229
PETACCIA <petaccia.seb@gmail.com>
2024-07-04 16:33:50 +0200

    Création du schema des catégories des ingredients et envoi de la data dans la base de données

---
+++ backend/data/categoryIngredientsData.js
@@ -0,0 +1,19 @@
+// Liste des catégories d'ingrédients
+const categoryIngredientsData = [
+  { name: "Viandes" },
+  { name: "Poissons" },
+  { name: "Légumes" },
+  { name: "Fruits" },
+  { name: "Céréales" },
+  { name: "Epicerie" },
+  { name: "Fruits secs" },
+  { name: "Herbes aromatiques" },
+  { name : "Produits laitiers" },
+  { name : "Huiles" },
+  { name : "Condiments" },
+  { name : "Vinaigre" },
+  { name: "Sauces" },
+  { name: "Beurre" },
+];
+
+module.exports = categoryIngredientsData;
