7614b2c9bbf93be32980ecc41e0dfbe6603f060d
PETACCIA <petaccia.seb@gmail.com>
2024-07-05 10:37:55 +0200

    Modification des data des recettes et des ingredients. Création des routes pour récupérer tous les ingredients ainsi que son controller

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const authRoutes = require("./authRoutes");
 const recipesRoutes = require("./recipesRoutes");
+const ingredientRoutes = require("./ingredientRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/recipes", recipesRoutes);
+router.use("/ingredients", ingredientRoutes);
 
 module.exports = router;
--- backend/src/seed/ingredients/seedIngredients.js
+++ backend/src/seed/ingredients/seedIngredients.js
@@ -1,21 +1,56 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const Ingredient = require('../../models/ingredientModel');
+const Ingredient = require('../../models/ingredients/ingredientModel');
+const Type = require('../../models/ingredients/typesModel');
+const Category = require('../../models/ingredients/categoriesModel');
 const connectDB = require('../../database/config');
 const ingredientsData = require('../../../data/ingredientsData');
-
 
 const createIngredients = async () => {
   try {
     await connectDB();
-    for (const ingredient of ingredientsData) {
-      const newIngredient = new Ingredient(ingredient);
-      await newIngredient.save();
-      console.log(`Enregistrement de l'ingrédient : ${newIngredient.name}`);
+
+    // Vider les collections
+    await Ingredient.deleteMany({});
+    await Type.deleteMany({});
+    await Category.deleteMany({});
+    console.log('Collections vidées');
+
+    // Enregistrer les types
+    const typesSet = new Set(ingredientsData.map(ingredient => ingredient.type));
+    for (const type of typesSet) {
+      const ingredientType = new Type({ name: type });
+      await ingredientType.save();
     }
-    console.log('Ingrédients enregistrées avec succès !');
+    console.log('Types enregistrés');
+
+    // Enregistrer les catégories
+    const categoriesSet = new Set(ingredientsData.map(ingredient => ingredient.category));
+    for (const category of categoriesSet) {
+      const ingredientCategory = new Category({ name: category });
+      await ingredientCategory.save();
+    }
+    console.log('Catégories enregistrées');
+
+    // Enregistrer les ingrédients
+    for (const ingredient of ingredientsData) {
+      // Trouver les ID des types et des catégories correspondants
+      const ingredientType = await Type.findOne({ name: ingredient.type });
+      const ingredientCategory = await Category.findOne({ name: ingredient.category });
+
+      // Enregistrer l'ingrédient
+      const newIngredient = new Ingredient({
+        ...ingredient,
+        // Ajouter l'ID des types et des catégories correspondants au nouvel ingrédient
+        type: ingredientType._id,
+        category: ingredientCategory._id,
+      });
+      await newIngredient.save();
+    }
+    console.log('Ingrédients enregistrés');
+
   } catch (error) {
-    console.error('Erreur lors de l\'enregistrement des ingrédients :', error);
+    console.error("Erreur lors de l'enregistrement des ingrédients :", error);
   } finally {
     mongoose.connection.close();
   }
