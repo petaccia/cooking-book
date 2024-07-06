ee0d7ce9ce234785401add9f5e8a568911f17799
PETACCIA <petaccia.seb@gmail.com>
2024-07-06 14:28:31 +0200

    Création du button de navigation pour se rédiriger vers la page CreateRecipe et intégration dans la page mybookCook

---
+++ frontend/src/pages/user/components/buttons/ButtonCreateRecipeNavigation/ButtonCreateRecipeNavigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ButtonCreateRecipeNavigation = () => {
+  return (
+    <>
+      <Link to="/user/recipe/createRecipe"
+        className="bg-orange-100 hover:bg-orange-800 hover:text-orange-100 text-sepia-800 font-bold py-2 px-4 rounded-md">
+        Créer une recette
+      </Link>
+    </>
+  )
+}
+
+export default ButtonCreateRecipeNavigation
